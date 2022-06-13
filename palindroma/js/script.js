078bd0f353b01a4fd708ce0c7b9ce52155e37cbd
Neo-Machina <andreistatache1997@gmail.com>
2022-06-13 17:35:31 +0200

    alpha exercise-1

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -1,2 +1,27 @@
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
+
+
+ 
+isPalindromaOrNot('otto');
+
+// Creazione parola che comprende se la parola inserita è palindroma
+// parola -> parola da verificare
+// return: booleano, true se la parola è palindroma altrimenti false 
+function isPalindromaOrNot(word) {
+    let palindromaWord = false;
+    // Scorriamo le lettere della parola
+    for (let i = word.length -1; i >= 0; i--) {
+        console.log(word[i]);
+        console.log(i);
+        const reverseName = word.length -1 - i;
+        console.log(reverseName);
+
+        if (word === reverseName) {
+            palindromaWord = true;
+        }
+
+        return palindromaWord;
+    }
+}
+
--- pari-e-dispari/js/script.js
+++ pari-e-dispari/js/script.js
@@ -3,3 +3,17 @@
 // Sommiamo i due numeri
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 // Dichiariamo chi ha vinto.
+
+// 
+
+
+// const randomOneToFive = generateRandomNumber(1, 5);
+// console.log(randomOneToFive);
+// // funzione che genera un numero random da 1 a 5
+// function generateRandomNumber (min, max) {
+//     const randomNumber = Math.floor(Math.random() * max) + min;
+
+//     return randomNumber;
+// }
+
+// 
