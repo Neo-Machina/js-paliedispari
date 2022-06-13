a058a0e1d7cc2247172e0aa4b3cc96a6522f17f1
Neo-Machina <andreistatache1997@gmail.com>
2022-06-14 00:47:48 +0200

    pari e dispari exercise completed

--- pari-e-dispari/js/script.js
+++ pari-e-dispari/js/script.js
@@ -4,16 +4,54 @@
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 // Dichiariamo chi ha vinto.
 
-// 
+// Scelta dell'utente tra pari e dispari 
+let evenOrOddUser = prompt('Scegli tra pari o dispari').toLowerCase();
 
+while(evenOrOddUser !== 'pari' && evenOrOddUser !== 'dispari') {
+    evenOrOddUser = prompt('Scegli tra pari o dispari').toLowerCase();
+}
 
-// const randomOneToFive = generateRandomNumber(1, 5);
-// console.log(randomOneToFive);
-// // funzione che genera un numero random da 1 a 5
-// function generateRandomNumber (min, max) {
-//     const randomNumber = Math.floor(Math.random() * max) + min;
+// Scelta dell'utente di un numero compreso tra 1 e 5
+let userNumber = parseInt( prompt('Dimmi un numero tra 1 e 5') );
 
-//     return randomNumber;
-// }
+while(userNumber < 1 || userNumber > 5) {
+    userNumber = parseInt( prompt('Dimmi un numero tra 1 e 5') );
+}
 
-// 
+// Variabile che rappresenta la somma tra il numero inserito dall'utente e un numero random 
+let sumNumbers = twoNumbersSum(userNumber, generateRandomNumber(1, 5));
+// Funzione che genera un numero random da 1 a 5
+function generateRandomNumber(min, max) {
+    const randomNumber = Math.floor(Math.random() * max) + min;
+
+    return randomNumber;
+}
+
+// Somma di due numeri
+function twoNumbersSum(num1, num2) {
+    const sum = num1 + num2;
+
+    return sum;
+}
+
+// Richiamo della function checkWinner nella const isUserWinner
+const isUserWinner = checkWinner(sumNumbers, evenOrOddUser);
+
+if (isUserWinner === true) {
+    alert(`Hai vinto !!! La somma dei due numeri è ${sumNumbers} e tu hai scelto ${evenOrOddUser}.`);
+} else {
+    alert(`Hai perso !!! La somma dei due numeri è ${sumNumbers} e tu hai scelto ${evenOrOddUser}.`);
+}
+
+// Stabilire se l'utente ha vinto o ha perso
+function checkWinner (number, choice) {
+    let userWinner = false;
+    // se la scelta dell'utente e la somma dei 2 numeri è pari
+    // oppure se la la scelta dell'utente e la somma dei 2 numeri è dispari, allora l'utente vince 
+    // altrimenti perde
+    if( (choice === 'pari' && number % 2 === 0) || (choice === 'dispari' && number % 2 !== 0) ) {
+        userWinner = true;
+    }
+
+    return userWinner;
+}
